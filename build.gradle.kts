393caeeca512f15c3ed72640f7dbeb247118de4a
Danilo Oliveira <danilomendoncaoliveira@gmail.com>
2026-04-24 09:16:03 +0200

    Upgrades kotlin and jetpack compose versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("multiplatform") version "2.1.0" apply false
-    kotlin("jvm") version "2.1.0" apply false
+    kotlin("multiplatform") version "2.2.20" apply false
+    kotlin("jvm") version "2.2.20" apply false
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.compose") version "1.7.3" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.compose") version "1.10.3" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.7" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
     jacoco
